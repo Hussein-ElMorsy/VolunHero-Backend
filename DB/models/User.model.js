63e19551f362631ef8c0921e10966cfa87cf01b5
Hussein Mohamed <114592524+Hussein-ElMorsy@users.noreply.github.com>
2024-02-04 01:44:32 +0200

    Huss

--- DB/models/User.model.js
+++ DB/models/User.model.js
@@ -1,69 +1,68 @@
 import mongoose, { Schema, Types, model } from "mongoose";
 
-
-const userSchema = new Schema({
-    firstName:String,
-    lastName:String,
+const userSchema = new Schema(
+  {
+    firstName: String,
+    lastName: String,
     userName: {
-        type: String,
-        required: [true, 'userName is required'],
-        min: [2, 'minimum length 2 char'],
-        max: [20, 'max length 2 char'],
-        lowercase:true
-
+      type: String,
+      required: [true, "userName is required"],
+      min: [2, "minimum length 2 char"],
+      max: [20, "max length 2 char"],
+      lowercase: true,
     },
     email: {
-        type: String,
-        unique: [true, 'email must be unique value'],
-        required: [true, 'userName is required'],
-        lowercase:true
+      type: String,
+      unique: [true, "email must be unique value"],
+      required: [true, "userName is required"],
+      lowercase: true,
     },
     password: {
-        type: String,
-        required: [true, 'password is required'],
+      type: String,
+      required: [true, "password is required"],
     },
     phone: {
-        type: String,
+      type: String,
     },
     role: {
-        type: String,
-        default: 'User',
-        enum: ['User', 'Admin']
+      type: String,
+      default: "User",
+      enum: ["User", "Admin"],
     },
-    status:{
-        type:String,
-        default: 'offline',
-        enum: ['offline', 'online','blocked']
-
-    }
-    ,
+    status: {
+      type: String,
+      default: "offline",
+      enum: ["offline", "online", "blocked"],
+    },
     confirmEmail: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     image: Object,
     DOB: String,
-    address:String,
-    gender:{
-        type:String,
-        default:"Male",
-        enum:["Male",'Female']
+    address: String,
+    gender: {
+      type: String,
+      default: "Male",
+      enum: ["Male", "Female"],
     },
-    forgetCode:{
-        type:Number,
-        default:null,
+    forgetCode: {
+      type: Number,
+      default: null,
     },
-    changePasswordTime:{
-        type:Date,
-        default:null
+    changePasswordTime: {
+      type: Date,
+      default: null,
     },
-    wishList:{
-        type:[{type:Types.ObjectId,ref:'Product'}]
-    }
-}, {
-    timestamps: true
-})
+    wishList: {
+      type: [{ type: Types.ObjectId, ref: "Product" }],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-
-const userModel = mongoose.models.User || model('User', userSchema);
-export default userModel
+const userModel = mongoose.models.User || model("User", userSchema);
+export default userModel;
+//huss
