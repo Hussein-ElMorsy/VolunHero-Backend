c580978fc3e5864ad437f7474920d08520d71f11
Seif Mahmoud <seifmahmoud871@gmail.com>
2024-04-26 17:28:55 +0300

    Your commit message

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -7,10 +7,10 @@ import { fileUpload, fileValidation } from "../../utils/multer.js";
 const router = Router();
 router.get("/",authController.getAuthModule);
 
-router.post('/signUp',fileUpload(fileValidation.image).single('image'),validation(validators.signUp),asyncHandler(authController.signUp))
-router.get("/confirmEmail/:token",validation(validators.token),asyncHandler(authController.confirmEmail))
-router.get('/newConfirmEmail/:token',validation(validators.token),asyncHandler(authController.newConfirmEmail))
-router.post('/login',validation(validators.login),asyncHandler(authController.login))
+router.post('/signUp',fileUpload(fileValidation.image).single('image'),validation(validators.signUp),asyncHandler(authController.signUp));
+router.get("/confirmEmail/:token",validation(validators.token),asyncHandler(authController.confirmEmail));
+router.get('/newConfirmEmail/:token',validation(validators.token),asyncHandler(authController.newConfirmEmail));
+router.post('/login',validation(validators.login),asyncHandler(authController.login));
 router.patch('/sendCode',validation(validators.sendCode),asyncHandler(authController.sendCode));
 router.put('/forgetPassword',validation(validators.forgetPassword),asyncHandler(authController.forgetPassword));
 
