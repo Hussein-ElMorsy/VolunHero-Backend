27542e445703a6563cfa435ac7066ff324ae8a32
Seif Mahmoud <seifmahmoud871@gmail.com>
2024-04-27 14:12:18 +0300

    edit auth

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -1,49 +1,64 @@
 import joi from "joi";
-import {generalFields} from "../../Middleware/validation.middleware.js"
+import { generalFields } from "../../Middleware/validation.middleware.js"
 
 export const signUp = joi.object({
-
-    firstName:joi.string().min(2).max(25),
-    lastName:joi.string().min(2).max(25),
-    userName:joi.string().min(2).max(25).required(),
-    email:generalFields.email.required(),
-    password:generalFields.password.required(),
-    cpassword:generalFields.cPassword.required(),
-    DOB: joi.date()
-    .max('now')
-    .custom((value, helpers) => {
+  firstName: joi.string().min(2).max(25),
+  lastName: joi.string().min(2).max(25),
+  userName: joi.string().min(2).max(25).required(),
+  email: generalFields.email.required(),
+  password: generalFields.password.required(),
+  cpassword: generalFields.cPassword.required(),
+  DOB: joi.date().max('now').custom((value, helpers) => {
       const age = new Date().getFullYear() - new Date(value).getFullYear();
       console.log(age);
       if (age < 18) {
-        return helpers.message('Age must be more than 18 years old');
+          return helpers.message('Age must be more than 18 years old');
       }
       return value;
-    }),
-    file:generalFields.file,
-    phone:generalFields.phone,
-    gender:generalFields.gender,
-    address:generalFields.address,
-
-
+  }),
+  role: generalFields.role,
+  specification: generalFields.specification,
+  file: joi.object({
+      attachments: joi.array().items(generalFields.file).max(10),
+  }),
+  phone: generalFields.phone,
+  gender: generalFields.gender,
+  address: generalFields.address,
+  overview: generalFields.overview,
+  website: generalFields.website,
+  headquarters: generalFields.headquarters,
+  specialties: generalFields.specialties,
+  locations: generalFields.locations,
 }).required();
 
-export const token=joi.object({
-  token:joi.string().required(),
+// export const finalSignUp = signUp.when('specification', {
+//   is: joi.valid('Medical', 'Educational'),
+//   then: joi.object({
+//     attachments: joi.array().items(generalFields.file).max(10).required()
+//   }).required(),
+//   otherwise: joi.object({
+//     attachments: joi.array().items(generalFields.file).max(10)
+//   })
+// }).required();
+
+
+export const token = joi.object({
+  token: joi.string().required(),
 }).required()
 
 export const login = joi.object({
-  email:generalFields.email.required(),
-  password:generalFields.password.required(),
+  email: generalFields.email.required(),
+  password: generalFields.password.required(),
 }).required()
 
 
 export const forgetPassword = joi.object({
-  email:generalFields.email.required(),
-  password:generalFields.password.required(),
-  cPassword:generalFields.cPassword.required(),
-  code:joi.string().pattern(new RegExp(/^[0-9]{4}$/)).min(4).max(4).required()
+  email: generalFields.email.required(),
+  password: generalFields.password.required(),
+  cPassword: generalFields.cPassword.required(),
+  code: joi.string().pattern(new RegExp(/^[0-9]{4}$/)).min(4).max(4).required()
 }).required()
 
 export const sendCode = joi.object({
-  email:generalFields.email.required(),
+  email: generalFields.email.required(),
 }).required()
