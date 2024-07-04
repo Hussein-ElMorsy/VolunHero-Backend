aafd873a544c5de940c7b640af8154ae3aa10089
Seif Mahmoud <seifmahmoud871@gmail.com>
2024-07-04 16:48:05 +0300

    edit in specifications

--- src/modules/specifications/controller/specifications.js
+++ src/modules/specifications/controller/specifications.js
@@ -2,15 +2,18 @@ import userModel from "../../../../DB/models/User.model.js"
 
 
 
-export const getUsers = async(req,res,next)=>{
+export const getUsers = async (req, res, next) => {
 
-    let {specification} = req.params;
-    specification= specification.charAt(0).toUpperCase()+specification.slice(1);
+    let { specification } = req.params;
+    specification = specification.charAt(0).toUpperCase() + specification.slice(1);
     console.log(specification);
-    const users = await userModel.find({specification,status:"online"})
-    .select("userName role status phone profilePic specification");
+    const users = await userModel.find({
+        _id: { $ne: req.user._id },
+        specification: specification,
+        status: "online",
+    }).select("userName role status phone profilePic specification");
 
-    return res.status(200).json({message:"success",users});
+    return res.status(200).json({ message: "success", users });
 
 }
 
